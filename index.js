043076a336da094bcb196bab34d29a4c48a4e35c
Sergio Perez <sergio13prez@gmail.com>
2019-08-01 00:08:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var newArray = [];
+  
+  for(var i = 0; i < musicians.length; i++){
+    newArray[i] = `${musicians[i]} plays ${instruments[i]}`;
+  }
+  
+  return newArray;
+}
+
+function johnLennonFacts(facts){
+  var i = 0;
+  while(i < facts.length){
+    facts[i] = `${facts[i]}!!!`;
+    i++;
+  }
+  
+  return facts;
+}
+
+function iLoveTheBeatles(number){
+  var newArray = [];
+  do{
+    newArray.push("I love the Beatles!")
+    number++
+  } while (number < 15);
+}
+
+console.log(iLoveTheBeatles(10))
